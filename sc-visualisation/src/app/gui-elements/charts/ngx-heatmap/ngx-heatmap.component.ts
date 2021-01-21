84fa3680246f70c00660675157872825864e5d1d
Jarema Radom <jaremaradom@gmail.com>
2021-01-21 23:37:48 +0100

    further work on heatmap

--- sc-visualisation/src/app/gui-elements/charts/ngx-heatmap/ngx-heatmap.component.ts
+++ sc-visualisation/src/app/gui-elements/charts/ngx-heatmap/ngx-heatmap.component.ts
@@ -7,26 +7,41 @@ import { multi } from '../../../../assets/data/data';
   styleUrls: ['./ngx-heatmap.component.scss']
 })
 export class NgxHeatmapComponent implements OnInit {
-  multi!: any[];
-  view: any[] = [700, 300];
-  legend: boolean = true;
-  showLabels: boolean = true;
+  data!: any[];
+  width = 562;
+  height = 630;
+  view: [number, number] = [this.width , this.height];
+  legend: boolean = false;
+  showLabels: boolean = false;
   animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Country';
-  yAxisLabel: string = 'Year';
+  xAxis: boolean = false;
+  yAxis: boolean = false;
+  showYAxisLabel: boolean = false;
+  showXAxisLabel: boolean = false;
 
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
   constructor() {
+    this.data = this.generateMockData()
+    let wut = this.data;
     Object.assign(this, {
-      multi
+      wut
     });
   }
+
+  generateMockData() {
+    let out = [];
+    for(let i = 0; i < 10; i++) {
+      let series = []
+      for(let j=0;j < 10; j++) {
+        series.push({name: j.toString(), value: Math.floor(Math.random() * (100))})
+      }
+      out.push({name: i.toString(), series: series})
+    }
+    return out
+  }
+
 
   ngOnInit(): void {
   }
